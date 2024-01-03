50a845e545ae0930a2e874ad9995a2e9f2db8d6e
Fenet-damena <fenetdamena74@gmail.com>
2024-01-03 13:08:06 -0800

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for l in range(0, 10):
+    for k in range(l, 1, 10):
+        if l == 8 and k == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(l, k), end='')
