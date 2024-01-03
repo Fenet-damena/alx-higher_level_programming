65edd2539aa2d27af5ac2c36716b0654e80213af
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2024-01-03 13:14:30 -0800

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for l in range(0, 10):
-    for k in range(l + 1, 10):
-        if l == 8 and k == 9:
+for i in range(0, 10):
+    for k in range(i + 1, 10):
+        if i == 8 and k == 9:
             print('89')
         else:
-            print('{}{}, '.format(l, k), end='')
+            print('{}{}, '.format(i, k), end='')
