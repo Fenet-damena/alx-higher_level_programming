3be54b6e6a9a214fd004c048939c365a518ff9f7
Fenet-damena <fenetdamena74@gmail.com>
2024-01-04 10:33:56 -0800

    import add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
-a = 1
-b = 2
-add.add(a, b)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    s = add(a, b)
+    print(("{} + {} = {}".format(a, b, s)
