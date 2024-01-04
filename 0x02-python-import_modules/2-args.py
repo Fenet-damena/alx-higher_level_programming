863928ad3ce6d345f843cfb70672482f9f310a07
Fenet-damena <fenetdamena74@gmail.com>
2024-01-04 11:09:04 -0800

    arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+arguments = sys.argv[1:]
+n = len(arguments)
+if n == 0:
+    print("0  arguments.")
+else:
+    print("{} arguments:".format{n))
+    for i, arg in enumerate(arguments, start=1):
+        print("{}: {}".format(i, arg))
