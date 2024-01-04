fe1fa41a32a26dd9d892a9fc2ddc565586671a69
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2024-01-04 11:58:51 -0800

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    leng = len(argv) - 1
-    if leng < 1:
+  from sys  import argv
+  leng = len(argv) - 1
+  if leng < 1:
         print("{} arguments.".format(leng))
-    elif leng = 1:
+  elif leng = 1:
         print("{} argument.".format(leng))
-    else:
+  else:
         print("{} arguments.".format(leng))
         for k in range(leng):
             print("{}: {}".format(k + 1, argv[i + 1]))
