92590fd5ec852e4bad6512309167739f771c19dd
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2024-01-04 11:40:32 -0800

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-n = len(arguments)
-if n == 0:
-    print("0  arguments.")
-else:
-    print("{} arguments:".format(n))
-    for i, arg in enumerate(arguments, start=1):
-        print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    import sys
+    leng = len(argv) - 1
+    if leng < 1:
+        print("{} arguments.".format(leng))
+    elif leng = 1:
+        print("{} argument.".format(leng))
+    else:
+        print("{} arguments.".format(leng))
+        for k in range(leng):
+            print("{}: {}".format(k + 1, argv[i + 1]))
