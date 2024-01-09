f7e285968645da925ba3687b4f5233740dc06d29
Fenet-damena <fenetdamena74@gmail.com>
2024-01-09 11:45:54 -0800

    alx

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for j in range(len(my_list)):
-        if my_list[j] % 2 == 0:
-            new_list.append(True)
+    s = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            s.append(True)
         else:
-            new_list.append(False)
-    return new_list
+            s.append(False)
+    return (s)
