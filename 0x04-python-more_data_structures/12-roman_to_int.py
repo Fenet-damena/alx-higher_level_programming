0d7d41e4e429d640b5c85aa1f3d868224706f3f7
Fenet-damena <fenetdamena74@gmail.com>
2024-01-10 11:49:28 -0800

    a

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-def subtract_from_list(list_num):
-    subtract_total = 0
-    max_value = max(list_num)
-
-    for num in list_num:
-        if max_value > num:
-            subtract_total += num
-
-    return (max_value - subtract_total)
-
-
-def roman_to_integer(roman_string):
-    if not roman_string:
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    if not isinstance(roman_string, str):
-        return 0
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numeral_keys = list(roman_numerals.keys())
+    result = 0
+    prev_value = 0
 
-    num = 0
-    last_numeral = 0
-    numeral_list = [0]
+    for char in roman_string:
+        value = roman_numerals.get(char, 0)
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
 
-    for character in roman_string:
-        for numeral in numeral_keys:
-            if numeral == character:
-                if roman_numerals.get(character) <= last_numeral:
-                    num += subtract_from_list(numeral_list)
-                    numeral_list = [roman_numerals.get(character)]
-                else:
-                    numeral_list.append(roman_numerals.get(character))
-
-                last_numeral = roman_numerals.get(character)
-
-    num += subtract_from_list(numeral_list)
-
-    return num
+    return result
