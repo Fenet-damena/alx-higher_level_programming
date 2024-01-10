0bd3e4b328a216e7d910f9ab7ee7d5cc0348217e
Fenet-damena <fenetdamena74@gmail.com>
2024-01-10 12:41:13 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
-import sys
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-args = sys.argv[1:]  # Exclude the script name from the arguments
-
-num_args = len(args)
-
-if num_args == 0:
-    print("0 arguments.")
-else:
-    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
