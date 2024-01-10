690b0326672eb1c779ee263d38a4b3b14186b705
Fenet-damena <fenetdamena74@gmail.com>
2024-01-10 11:09:59 -0800

    more alx

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    n = matrix.copy()
+
+    for i in range(len(matrix)):
+        n[i] = list(map(lambda x: x**2, matrix[i]
+    return (n)
