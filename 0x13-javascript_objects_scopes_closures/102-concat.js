af6522a7c20a0ec817c003fec943aef2ab0f9bd9
Fenet-damena <fenetdamena74@gmail.com>
2024-05-04 09:27:23 -0700

    day2_js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-let dataFile1 = '';
-let dataFile2 = '';
-//  Gathering data 📀
-try {
-  dataFile1 = fs.readFileSync(process.argv[2], 'utf8');
-  dataFile2 = fs.readFileSync(process.argv[3], 'utf-8');
-} catch (err) {
-  console.error(err);
+if (process.argv[2] && process.argv[3] && process.argv[4]) {
+  const content1 = fs.readFileSync(process.argv[2], 'utf8');
+  fs.appendFile(process.argv[4], content1, function (err) { if (err) throw err; });
+  const content2 = fs.readFileSync(process.argv[3], 'utf8');
+  fs.appendFile(process.argv[4], content2, function (err) { if (err) throw err; });
 }
-// Writting file 📂
-const writeToFile = (data) => {
-  fs.appendFile(process.argv[4], data + '\n', (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-};
-writeToFile(dataFile1);
-writeToFile(dataFile2);
